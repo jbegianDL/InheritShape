3d2753d0888e6bf7c4cb287f7f8a025e2ce90a40
Jordan Begian <jordanbegian@Jordans-MacBook-Pro.local>
2019-02-04 09:15:25 -0500

    create circle constructor

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -1,5 +1,18 @@
 package com.company;
 
 public class Circle extends Shape {
+    private double radius;
 
+    public Circle(double radius, String color, boolean filled){
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
